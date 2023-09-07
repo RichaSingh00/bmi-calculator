8c1f4a2f302fa136bf5ebcaca3ca2d569dd01ef9
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:55:15 +0530

    Add project files.

---
+++ Model/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp.Model
+{
+    internal class Person
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly int _age;
+        private double _height;
+        private double _weight;
+        public Person(int id, string name, int age)
+        {
+            _id = id;
+            _name = name;
+            _age = age;
+            _height = 5;
+            _weight = 50;
+        }
+        public Person(int id, string name, int age, double height, double weight) : this(id, name, age)
+        {
+            _height = height;
+            _weight = weight;
+        }
+        public int GetId { get { return _id; } }
+        public string GetName { get { return _name; } }
+        public int GetAge { get { return _age; } }
+        public double Height { get { return _height; } }
+        public double Weight { get { return _weight; } }
+        public double CalculateTheBMI()
+        {
+            return (_weight / _height/ _height)*10;
+        }
+        public string GetBodyType()
+        {
+            double bmi = CalculateTheBMI();
+            double underWeight = 18.5;
+            double normalWeight = 25;
+            double overWeight = 30;
+            
+            if (bmi < underWeight)
+            {
+                return "Underweight";
+            }
+            else if (bmi >= underWeight && bmi < normalWeight)
+            {
+                return "Normal";
+            }
+            else if (bmi >= normalWeight && bmi < overWeight)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obese";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using PersonApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Jassie", 23, 5.3, 45);
+            Person person2 = new Person(2, "Stacy", 25);
+
+            Console.WriteLine("BMI for "+person1.GetName+" is:"+person1.CalculateTheBMI()+" and " +
+                "their BodyType is:"+person1.GetBodyType());
+            Console.WriteLine("BMI for " + person2.GetName + " is:" + person2.CalculateTheBMI() + 
+                " and their BodyType is:" + person2.GetBodyType());
+
+        }
+    }
+}
